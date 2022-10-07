262f94365713dc29da2a99eddae73d8f4ca5a5b8
MartynaM97 <110252592+MartynaM97@users.noreply.github.com>
2022-10-07 16:48:46 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from "./Container/Container";
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+
+function App() {
+  return (
+    <Container>
+      <Stopwatch />
+    </Container>
+  );
+}
+
+export default App;
